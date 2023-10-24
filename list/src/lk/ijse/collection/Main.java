c038ef9eb631546d4fc398a00d1ce89d9ececaba
danu <danujagreru@gmail.com>
2023-10-24 13:31:38 +0530

    initial commit

---
+++ list/src/lk/ijse/collection/Main.java
@@ -0,0 +1,12 @@
+package lk.ijse.collection;
+
+/*
+    @author DanujaV
+    @created 10/24/23 - 1:31 PM   
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Collection API");
+    }
+}
